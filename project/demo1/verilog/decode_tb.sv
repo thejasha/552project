08df382e9dc07f34f1e30f64b515284b02bc8069
Josh A <jamann2202@gmail.com>
2024-10-23 22:11:26 -0500

    Decode almost finsihed

--- project/demo1/verilog/decode_tb.sv
+++ project/demo1/verilog/decode_tb.sv
@@ -2,19 +2,19 @@ module decode_tb();
 
     // Declare signals
 
-    logic [15:0] instruction, read_data_1, read_data_2, write_data, i_1, i_2, word_align_jump;
+    logic [15:0] instruction, read_data_1, read_data_2, i_1, i_2, word_align_jump, to_shift, data_write;
 
     // Instantiate the DUT (Device Under Test)
-    decode iDUT (.instruction(instruction), .read_data_1(read_data_1), .read_data_2(read_data_2), .write_data(write_data), .i_1(i_1), .i_2(i_2), .word_align_jump(word_align_jump));
+    decode iDUT (.instruction(instruction), .read_data_1(read_data_1), .read_data_2(read_data_2), .i_1(i_1), .i_2(i_2), .word_align_jump(word_align_jump),  .to_shift(to_shift), .data_write(data_write));
 
 
     initial begin
         
-    instruction = 16'h1629;
+    instruction = 16'h2001;
+    data_write = 0;
     #5;
-     instruction = 16'h1699;
+    instruction = 16'b0101100000010000;
     #5;
-    
     end
 
 endmodule
